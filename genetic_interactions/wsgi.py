44ea1483fd75cf4a217655beca9c4817d49aff16
Katherine Erickson <katherine.erickson@gmail.com>
2014-05-19 14:24:08 -0400

    PEP8 style conventions

--- genetic_interactions/wsgi.py
+++ genetic_interactions/wsgi.py
@@ -8,7 +8,8 @@ https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "genetic_interactions.settings")
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "genetic_interactions.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
