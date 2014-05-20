3c2e623bf802140771ac95452a549ba81ec21719
Katherine Erickson <katherine.erickson@gmail.com>
2014-05-20 17:52:03 -0400

    reference eegi settings in manage.py

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "genetic_interactions.settings")
+        "DJANGO_SETTINGS_MODULE", "eegi.settings")
 
     from django.core.management import execute_from_command_line
 
