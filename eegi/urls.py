8d134ae1b48ec90f4577a2199e99bed013146c84
Katherine Erickson <katherine.erickson@gmail.com>
2014-08-27 16:34:21 -0400

    auto discover admin; clones->library

--- eegi/urls.py
+++ eegi/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 
+admin.autodiscover()
+
+
 urlpatterns = patterns(
     '',
     url(r'', include('website.urls')),
