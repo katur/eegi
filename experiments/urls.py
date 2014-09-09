64b90d815c282b235895803ce8d58ad6a980e113
Katherine Erickson <katherine.erickson@gmail.com>
2014-09-09 15:47:41 -0400

    work on experiment pages

--- experiments/urls.py
+++ experiments/urls.py
@@ -3,6 +3,9 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'experiments.views',
-    url(r'^experiments/(?P<start>[0-9]+)/(?P<end>[0-9]+)$', 'experiments',
-        name='experiments_url'),
+    url(r'^experiments$', 'experiments', name='experiments_url'),
+    url(r'^experiment/(?P<id>[0-9]+)$', 'experiment',
+        name='experiment_url'),
+    url(r'^experiment-range/(?P<start>[0-9]+)/(?P<end>[0-9]+)$',
+        'experiment_range', name='experiment_range_url'),
 )
--- experiments/views.py
+++ experiments/views.py
@@ -1,15 +1,63 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from experiments.models import Experiment
 
 
-def experiments(request, start, end):
+def experiments(request, context=None):
+    if 'id' in request.GET:
+        try:
+            id = int(request.GET['id'])
+            if (id < 1):
+                error_message = 'Experiment id must be positive'
+            else:
+                return redirect(experiment, id=id)
+
+        except ValueError:
+            error_message = 'Experiment id must be a positive integer'
+
+        context = {'id_error_message': error_message}
+        return render(request, 'experiments.html', context)
+
+    elif 'start' in request.GET and 'end' in request.GET:
+        try:
+            start = int(request.GET['start'])
+            end = int(request.GET['end'])
+
+            if (start < 1) or (end < 1):
+                error_message = ('Start and end experiment ids '
+                                 'must be positive')
+            elif start > end:
+                error_message = ('Start must be greater than '
+                                 'or equal to end')
+            elif (end - start) > 1001:
+                error_message = ('Range must be 1000 items or less')
+            else:
+                return redirect(experiment_range, start=start, end=end)
+
+        except ValueError:
+            error_message = ('Start and end experiment ids '
+                             'must be positive integers')
+
+        context = {'range_error_message': error_message}
+        return render(request, 'experiments.html', context)
+
+    else:
+        return render(request, 'experiments.html', context)
+
+
+def experiment(request, id):
+    experiment = get_object_or_404(Experiment, pk=id)
+    context = {'experiment': experiment}
+    return render(request, 'experiment.html', context)
+
+
+def experiment_range(request, start, end):
     experiments = (
         Experiment.objects
-        .filter(id__gte=start).filter(id__lt=end)
+        .filter(id__gte=start).filter(id__lte=end)
         .values('id', 'worm_strain', 'worm_strain__genotype',
                 'library_plate', 'temperature', 'date', 'is_junk',
                 'comment')
     )
 
-    context = {'experiments': experiments}
-    return render(request, 'experiments.html', context)
+    context = {'experiments': experiments, 'start': start, 'end': end}
+    return render(request, 'experiments_range.html', context)
