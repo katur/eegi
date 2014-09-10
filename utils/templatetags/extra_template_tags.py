47d6ce7fe9cb130fa6406699a86e2f1436b6307d
Katherine Erickson <katherine.erickson@gmail.com>
2014-09-10 18:17:39 -0400

    add experiment images to the experiment page

--- utils/templatetags/extra_template_tags.py
+++ utils/templatetags/extra_template_tags.py
@@ -11,8 +11,21 @@ def get_celsius_string(temperature):
         return None
 
 
+@register.filter
+def get_range(value):
+    return range(value)
+
+
 @register.simple_tag
 def url_replace(request, field, value):
     query_dict = request.GET.copy()
     query_dict[field] = value
     return query_dict.urlencode()
+
+
+@register.simple_tag
+def get_image_url(experiment, tile_number):
+    prefix = 'http://pleiades.bio.nyu.edu/GI_IMG/'
+    tile = 'Tile0000{}.bmp'.format(str(tile_number).zfill(2))
+    url = '{}{}/{}'.format(prefix, experiment.id, tile)
+    return url
