6077598d8ae93808f45c918b95addc2569131544
Katherine Erickson <katherine.erickson@gmail.com>
2014-09-10 18:34:45 -0400

    create separate step in well/tile conversion for getting a plate index (0-95)

--- utils/helpers/well_tile_conversion.py
+++ utils/helpers/well_tile_conversion.py
@@ -6,37 +6,55 @@ NUMBER_OF_COLUMNS = 12
 
 
 def well_to_tile(well):
-    assert re.match('[A-H]\d\d', well)
+    index = well_to_index(well)
+    return index_to_tile(index)
+
+
+def tile_to_well(tile):
+    index = tile_to_index(tile)
+    return index_to_well(index)
+
+
+def index_to_tile(index):
+    return 'Tile0000{}.bmp'.format(str(index + 1).zfill(2))
+
+
+def tile_to_index(tile):
+    if not re.match('Tile0000\d\d.bmp', tile):
+        raise ValueError('Improper tile string')
+    index = int(tile[8:10]) - 1
+    return index
+
+
+def index_to_well(index):
+    row = rows[index / NUMBER_OF_COLUMNS]
+    index_in_row = index % NUMBER_OF_COLUMNS
+
+    if row in backwards_rows:
+        position_from_left = NUMBER_OF_COLUMNS - 1 - index_in_row
+    else:
+        position_from_left = index_in_row
+
+    column = position_from_left + 1
+    return '{}{}'.format(row, str(column).zfill(2))
+
+
+def well_to_index(well):
+    if not re.match('[A-H]\d\d', well):
+        raise ValueError('Improper well string')
     row = well[0]
     column = int(well[1:3])
     position_from_left = column - 1
     assert position_from_left >= 0 and position_from_left < NUMBER_OF_COLUMNS
 
-    min_row_rank = (ord(row) - 65) * NUMBER_OF_COLUMNS
+    min_row_index = (ord(row) - 65) * NUMBER_OF_COLUMNS
     if row in backwards_rows:
-        rank_in_row = NUMBER_OF_COLUMNS - 1 - position_from_left
+        index_in_row = NUMBER_OF_COLUMNS - 1 - position_from_left
     else:
-        rank_in_row = position_from_left
+        index_in_row = position_from_left
 
-    overall_rank = min_row_rank + rank_in_row
-
-    return 'Tile0000{0}.bmp'.format(
-        str(overall_rank + 1).zfill(2))
-
-
-def tile_to_well(tile):
-    assert re.match('Tile0000\d\d.bmp', tile)
-    overall_rank = int(tile[8:10]) - 1
-    row = rows[overall_rank / NUMBER_OF_COLUMNS]
-    rank_in_row = overall_rank % NUMBER_OF_COLUMNS
-
-    if row in backwards_rows:
-        position_from_left = NUMBER_OF_COLUMNS - 1 - rank_in_row
-    else:
-        position_from_left = rank_in_row
-
-    column = position_from_left + 1
-    return '{}{}'.format(row, str(column).zfill(2))
+    overall_index = min_row_index + index_in_row
+    return overall_index
 
 
 if __name__ == '__main__':
