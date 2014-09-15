2edad10a449306d6525bb2c329dfec89d06e89c2
Katherine Erickson <katherine.erickson@gmail.com>
2014-09-15 11:23:33 -0400

    more explanation on experiment form fields

--- experiments/forms.py
+++ experiments/forms.py
@@ -5,13 +5,14 @@ class ExperimentFilterForm(forms.Form):
     """
     Form for filtering Experiment instances.
     """
-    id__gte = forms.IntegerField(required=False, label="Min id")
-    id__lte = forms.IntegerField(required=False, label="Max id")
+    id__gte = forms.IntegerField(required=False, label='Min id (inclusive)')
+    id__lte = forms.IntegerField(required=False, label='Max id (inclusive)')
     worm_strain = forms.CharField(required=False)
     worm_strain__gene = forms.CharField(required=False)
     worm_strain__allele = forms.CharField(required=False)
     library_plate = forms.CharField(required=False)
-    temperature = forms.DecimalField(required=False)
+    temperature = forms.DecimalField(required=False,
+                                     label='Temperature (number only)')
     date = forms.DateField(required=False,
                            label="Date (YYYY-MM-DD)")
     is_junk = forms.NullBooleanField(required=False)
