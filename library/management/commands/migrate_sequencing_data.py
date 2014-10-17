7f111d57c1a51f427519b41b49fb375b41b1a983
Katherine Erickson <katherine.erickson@gmail.com>
2014-10-17 17:48:52 -0400

    add parsing excel xls file for sequencing results, if txt does not exist.

--- library/management/commands/migrate_sequencing_data.py
+++ library/management/commands/migrate_sequencing_data.py
@@ -1,6 +1,8 @@
 import csv
 import glob
 import sys
+import xlrd
+import datetime
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.management.base import BaseCommand, CommandError
@@ -75,82 +77,120 @@ class Command(BaseCommand):
 
 
 def update_LibrarySequencing_table(genewiz_output_root, tracking_number):
-    qscrl_filepath = ('{}/{}_QSCRL.txt'.format(genewiz_output_root,
-                                               tracking_number))
+    qscrl_txt = ('{}/{}_QSCRL.txt'.format(genewiz_output_root,
+                                          tracking_number))
+    qscrl_xls = ('{}/{}_QSCRL.xls'.format(genewiz_output_root,
+                                          tracking_number))
     try:
-        qscrl_file = open(qscrl_filepath, 'rb')
+        qscrl_file = open(qscrl_txt, 'rb')
+        with qscrl_file:
+            qscrl_reader = csv.DictReader(qscrl_file, delimiter='\t')
+            for row in qscrl_reader:
+                # Need tracking number and tube label because they are the
+                # fields that genewiz uses to uniquely define sequences,
+                # in the case of resequencing.
+                process_qscrl_row(row, genewiz_output_root, tracking_number)
 
-    except IOError as e:
-        sys.stderr.write('QSCRL file missing or could not be open '
-                         'for tracking number {}. I/O error({}): {}\n'
-                         .format(tracking_number, e.errno, e.strerror))
-        return
+    except IOError:
+        try:
+            book = xlrd.open_workbook(qscrl_xls, on_demand=True)
+            sheet = book.sheet_by_name(tracking_number)
+            keys = [sheet.cell(4, col_index).value
+                    for col_index in xrange(sheet.ncols)]
+            for row_index in xrange(5, sheet.nrows):
+                row = {}
+                for col_index in xrange(sheet.ncols):
+                    # cell_type = sheet.cell_type(row_index, col_index)
+                    cell_value = sheet.cell_value(row_index, col_index)
 
-    with qscrl_file:
-        qscrl_reader = csv.DictReader(qscrl_file, delimiter='\t')
-        for row in qscrl_reader:
-            # Need tracking number and tube label because they are the fields
-            # that genewiz uses to uniquely define sequences, in the case of
-            # resequencing.
-            tracking_number = row['trackingNumber']
-            tube_label = row['TubeLabel']
-            dna_name = row['DNAName']
+                    '''
+                    if cell_type == xlrd.XL_CELL_DATE:
+                        dt_tuple = xlrd.xldate_as_tuple(cell_value,
+                                                        book.datemode)
+                        cell_value = datetime.datetime(
+                            dt_tuple[0], dt_tuple[1], dt_tuple[2],
+                            dt_tuple[3], dt_tuple[4], dt_tuple[5]
+                        )
+                    '''
+                    row[keys[col_index]] = cell_value
 
-            # Need sample plate namd and sample tube number because they are
-            # what we name our samples (to identify what sequence well came
-            # from what library well).
-            # Note that tube_label can't be used for sample_tube_number
-            # because it is often 1-2 instead of 95-96
-            sample_plate_name = get_plate_name_from_dna_name(dna_name)
-            sample_tube_number = get_tube_number_from_dna_name(dna_name)
+                process_qscrl_row(row, genewiz_output_root, tracking_number)
+            '''
+            num_rows = worksheet.nrows - 1
+            curr_row = 3
+            while curr_row < num_rows:
+                curr_row += 1
+                row = worksheet.row(curr_row)
+                print row
+            '''
 
-            timestamp = row['Created_Dttm']
+        except IOError as e:
+            sys.stderr.write('QSCRL file missing or could not be open '
+                             'for tracking number {}. I/O error({}): {}\n'
+                             .format(tracking_number, e.errno, e.strerror))
+            return
 
-            # avoid Template_Name... sometimes e.g. 'GC1'
 
-            if '_R' in tube_label:
-                dna_name += '_R'
+def process_qscrl_row(row, genewiz_output_root, tracking_number):
+    tracking_number = row['trackingNumber']
+    tube_label = row['TubeLabel']
+    dna_name = row['DNAName']
 
-            seq_filepath = ('{}/{}_seq/{}_*.seq'.format(genewiz_output_root,
-                                                        tracking_number,
-                                                        dna_name))
-            try:
-                seq_file = open(glob.glob(seq_filepath)[0], 'rb')
+    # Need sample plate namd and sample tube number because they are
+    # what we name our samples (to identify what sequence well came
+    # from what library well).
+    # Note that tube_label can't be used for sample_tube_number
+    # because it is often 1-2 instead of 95-96
+    sample_plate_name = get_plate_name_from_dna_name(dna_name)
+    sample_tube_number = get_tube_number_from_dna_name(dna_name)
 
-            except IOError:
-                sys.stderr.write('Seq file missing for tracking number '
-                                 '{}, dna {}\n'
-                                 .format(tracking_number, dna_name))
-            with seq_file:
-                ab1_filename = seq_file.next()
-                ab1_filename = ab1_filename.strip()
-                ab1_filename = ab1_filename.split('>')[1]
+    # timestamp = row['Created_Dttm']
 
-                sequence = ''
-                for seq_row in seq_file:
-                    sequence += seq_row.strip()
+    # avoid Template_Name... sometimes e.g. 'GC1'
 
-            try:
-                LibrarySequencing.objects.get(
-                    genewiz_tracking_number=tracking_number,
-                    genewiz_tube_label=tube_label)
-            except ObjectDoesNotExist:
-                new_sequence = LibrarySequencing(
-                    sample_plate_name=sample_plate_name,
-                    sample_tube_number=sample_tube_number,
-                    genewiz_tracking_number=tracking_number,
-                    genewiz_tube_label=tube_label,
-                    sequence=sequence,
-                    ab1_filename=ab1_filename,
-                    quality_score=row['QualityScore'],
-                    crl=row['CRL'],
-                    qv20plus=row['QV20Plus'],
-                    si_a=row['SI_A'],
-                    si_c=row['SI_C'],
-                    si_g=row['SI_G'],
-                    si_t=row['SI_T']
-                )
-                new_sequence.save()
+    if '_R' in tube_label:
+        dna_name += '_R'
+
+    seq_filepath = ('{}/{}_seq/{}_*.seq'.format(genewiz_output_root,
+                                                tracking_number,
+                                                dna_name))
+    try:
+        seq_file = open(glob.glob(seq_filepath)[0], 'rb')
+
+    except IOError:
+        sys.stderr.write('Seq file missing for tracking number '
+                         '{}, dna {}\n'
+                         .format(tracking_number, dna_name))
+    with seq_file:
+        ab1_filename = seq_file.next()
+        ab1_filename = ab1_filename.strip()
+        ab1_filename = ab1_filename.split('>')[1]
+
+        sequence = ''
+        for seq_row in seq_file:
+            sequence += seq_row.strip()
+
+    try:
+        LibrarySequencing.objects.get(
+            genewiz_tracking_number=tracking_number,
+            genewiz_tube_label=tube_label)
+    except ObjectDoesNotExist:
+        new_sequence = LibrarySequencing(
+            sample_plate_name=sample_plate_name,
+            sample_tube_number=sample_tube_number,
+            genewiz_tracking_number=tracking_number,
+            genewiz_tube_label=tube_label,
+            sequence=sequence,
+            ab1_filename=ab1_filename,
+            quality_score=row['QualityScore'],
+            crl=row['CRL'],
+            qv20plus=row['QV20Plus'],
+            si_a=row['SI_A'],
+            si_c=row['SI_C'],
+            si_g=row['SI_G'],
+            si_t=row['SI_T']
+        )
+        new_sequence.save()
 
 
 def get_plate_name_from_dna_name(dna_name):
