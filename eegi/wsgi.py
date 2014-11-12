d4a90df23a2ce7a2157be2687e08044804f4761e
Katherine Erickson <katherine.erickson@gmail.com>
2014-11-12 11:25:06 -0500

    add static file serving with dj-static

--- eegi/wsgi.py
+++ eegi/wsgi.py
@@ -12,4 +12,5 @@ os.environ.setdefault(
     "DJANGO_SETTINGS_MODULE", "eegi.settings")
 
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+from dj_static import Cling
+application = Cling(get_wsgi_application())
