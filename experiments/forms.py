a01897ce1e06a80b9381853303897b8b483dac97
Katherine Erickson <katherine.erickson@gmail.com>
2014-11-14 19:11:39 -0500

    styling; cleaning up style code

--- experiments/forms.py
+++ experiments/forms.py
@@ -5,16 +5,21 @@ class ExperimentFilterForm(forms.Form):
     """
     Form for filtering Experiment instances.
     """
-    id__gte = forms.IntegerField(required=False, label='Min id (inclusive)')
-    id__lte = forms.IntegerField(required=False, label='Max id (inclusive)')
+    id__gte = forms.IntegerField(required=False,
+                                 label='Min id',
+                                 help_text='inclusive')
+    id__lte = forms.IntegerField(required=False,
+                                 label='Max id',
+                                 help_text='inclusive')
     worm_strain = forms.CharField(required=False)
     worm_strain__gene = forms.CharField(required=False)
     worm_strain__allele = forms.CharField(required=False)
     library_plate = forms.CharField(required=False)
     temperature = forms.DecimalField(required=False,
-                                     label='Temperature (number only)')
+                                     help_text='number only')
     date = forms.DateField(required=False,
-                           label="Date (YYYY-MM-DD)")
+                           label='Date',
+                           help_text='YYYY-MM-DD')
     is_junk = forms.NullBooleanField(required=False)
 
 
@@ -22,10 +27,10 @@ class DoubleKnockdownForm(forms.Form):
     """
     Form for finding a double knockdown.
     """
-    query = forms.CharField(
-        label='Mutant query', help_text='gene, allele, or strain name')
-    target = forms.CharField(
-        label='RNAi Target', help_text='gene, cosmid, clone, or position')
+    query = forms.CharField(label='Mutant query',
+                            help_text='gene, allele, or strain name')
+    target = forms.CharField(label='RNAi Target',
+                             help_text='gene, cosmid, clone, or position')
     screen = forms.ChoiceField(
         choices=[('SUP', 'suppressor'), ('ENH', 'enhancer')],
         widget=forms.RadioSelect)
