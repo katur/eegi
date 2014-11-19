8317c9bd52ed8cf7f316978fadf59f138f3ab7fb
Katherine Erickson <katherine.erickson@gmail.com>
2014-11-19 11:57:40 -0500

    move logic of choosing worm strain's url from template to view

--- worms/views.py
+++ worms/views.py
@@ -7,5 +7,11 @@ def worm_strains(request):
     Render the page that shows all worm strains used in the screen.
     """
     worm_strains = WormStrain.objects.all()
+    for strain in worm_strains:
+        if request.user.is_authenticated():
+            strain.url = strain.get_lab_website_url()
+        else:
+            strain.url = strain.get_wormbase_url()
+
     context = {'strains': worm_strains}
     return render(request, 'worm_strains.html', context)
