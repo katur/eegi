3c64101c626724a8738690736c18ee604cc09a7e
Katherine Erickson <katherine.erickson@gmail.com>
2014-11-19 12:16:40 -0500

    remove named parameters

--- library/urls.py
+++ library/urls.py
@@ -4,6 +4,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'library.views',
     url(r'^library-plates$', 'library_plates', name='library_plates_url'),
-    url(r'^library-plate/(?P<id>.*)$', 'library_plate',
-        name='library_plate_url'),
+    url(r'^library-plate/([^/]*)$', 'library_plate', name='library_plate_url'),
 )
