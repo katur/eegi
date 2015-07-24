9e15b9facd903aa3a1f26de427f6928305024004
Katherine Erickson <katherine.erickson@gmail.com>
2015-07-24 17:50:16 -0400

    More work on clone models draft

--- clones/models.py
+++ clones/models.py
@@ -4,13 +4,13 @@ from django.db import models
 class Clone(models.Model):
     """An RNAi clone used in the screen."""
     id = models.CharField(max_length=30, primary_key=True)
-    """
+    '''
     antiquated_pk = models.CharField(max_length=30, blank=True)
     library = models.CharField(max_length=30, blank=True)
     clone_type = models.CharField(max_length=30, blank=True)
     forward_primer = models.CharField(max_length=100, blank=True)
     reverse_primer = models.CharField(max_length=100, blank=True)
-    """
+    '''
 
     class Meta:
         db_table = 'RNAiClone'
@@ -24,9 +24,24 @@ class Clone(models.Model):
 
 
 '''
+class CloneAlias(models.Model):
+    clone = models.ForeignKey(Clone)
+    alias = models.CharField(max_length=30)
+    alias_type = models.CharField(max_length=30, blank=True)
+    source = models.CharField(max_length=30, blank=True)
+
+
 class Gene(models.Model):
     id = models.CharField(max_length=30, primary_key=True)
     cosmid_id = models.CharField(max_length=30)
+    locus = models.CharField(max_length=30, blank=True)
+    gene_type = models.CharField(max_length=30, blank=True)
+
+
+class GeneAlias(models.Model):
+    gene = models.ForeignKey(Gene)
+    alias = models.CharField(max_length=100)
+    alias_type = models.CharField(max_length=100, blank=True)
 
 
 class CloneTarget(models.Model):
@@ -41,4 +56,8 @@ class CloneTarget(models.Model):
     raw_score = models.IntegerField()
     unique_raw_score = models.IntegerField()
     relative_score = models.FloatField()
+    specificity_index = models.FloatField()
+    unique_chunk_index = models.FloatField()
+    is_on_target = models.BooleanField()
+    is_primary_target = models.BooleanField()
 '''
