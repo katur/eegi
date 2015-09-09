187f00a98c8ca9d95ce254b2032406eaba3a09e5
Katherine Erickson <katherine.erickson@gmail.com>
2015-09-09 17:08:35 -0400

    Add devstar image to experiment_well page

--- utils/templatetags/extra_template_tags.py
+++ utils/templatetags/extra_template_tags.py
@@ -1,3 +1,4 @@
+import urllib2
 import string
 from decimal import Decimal
 
@@ -53,21 +54,47 @@ def get_screen_type(temperature, strain):
         return 'not SUP or ENH screening temperature for this strain'
 
 
+IMG_SERVER = 'http://pleiades.bio.nyu.edu'
+IMG_PATH = IMG_SERVER + '/GI_IMG'
+THUMBNAIL_PATH = IMG_SERVER + '/GI_IMG/convertedImg'
+DEVSTAR_PATH = IMG_SERVER + '/GI_IMG/resultimages'
+
+
 @register.simple_tag
 def get_image(experiment, well):
-    prefix = 'http://pleiades.bio.nyu.edu/GI_IMG/'
     tile = well.get_tile()
-    url = '{}{}/{}'.format(prefix, experiment.id, tile)
+    url = '/'.join((IMG_PATH, str(experiment.id), tile))
     return url
 
 
 @register.simple_tag
 def get_thumbnail_image(experiment, well):
-    prefix = 'http://pleiades.bio.nyu.edu/GI_IMG/convertedImg/'
     tile = well.get_tile()
-    url = '{}{}/{}'.format(prefix, experiment.id, tile)
+    url = '/'.join((THUMBNAIL_PATH, str(experiment.id), tile))
     url = string.replace(url, 'bmp', 'jpg')
     return url
+
+
+def http_response_ok(url):
+    try:
+        r = urllib2.urlopen(url)
+    except urllib2.URLError as e:
+        return False
+    if r.code == 200:
+        return True
+    else:
+        return False
+
+
+@register.assignment_tag
+def get_devstar_image(experiment, well):
+    tile = well.get_tile()
+    url = '/'.join((DEVSTAR_PATH, str(experiment.id), tile))
+    url = string.replace(url, '.bmp', 'res.png')
+    if http_response_ok(url):
+        return url
+    else:
+        return None
 
 
 @register.simple_tag
