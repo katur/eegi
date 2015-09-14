ad92496e12521a62927a62fe03dadeb0b20388f7
Katherine Erickson <katherine.erickson@gmail.com>
2015-09-14 15:39:49 -0400

    Add template tag for generating <td> images

--- website/templatetags/image_tags.py
+++ website/templatetags/image_tags.py
@@ -11,6 +11,11 @@ register = template.Library()
 
 @register.simple_tag
 def get_image_url(experiment, well):
+    """Get the url of an experiment image.
+
+    Accepts an Experiment object, and well as a string.
+
+    """
     tile = well_to_tile(well)
     url = '/'.join((IMG_PATH, str(experiment.id), tile))
     return url
@@ -18,6 +23,11 @@ def get_image_url(experiment, well):
 
 @register.simple_tag
 def get_thumbnail_url(experiment, well):
+    """Get the url of an experiment thumbnail image.
+
+    Accepts an Experiment object, and well as a string.
+
+    """
     tile = well_to_tile(well)
     url = '/'.join((THUMBNAIL_PATH, str(experiment.id), tile))
     url = string.replace(url, 'bmp', 'jpg')
@@ -26,6 +36,11 @@ def get_thumbnail_url(experiment, well):
 
 @register.simple_tag
 def get_devstar_image_url(experiment, well):
+    """Get the url of a DevStaR output image.
+
+    Accepts an Experiment object, and well as a string.
+
+    """
     tile = well_to_tile(well)
     url = '/'.join((DEVSTAR_PATH, str(experiment.id), tile))
     url = string.replace(url, '.bmp', 'res.png')
@@ -34,11 +49,11 @@ def get_devstar_image_url(experiment, well):
 
 @register.assignment_tag
 def get_devstar_image_url_if_exists(experiment, well):
-    """Returns the DevStaR image output, or None if the url does not return
-    a 200 HTTP response.
+    """Get the url of a DevStaR output image, or None if it doesn't exist.
 
-    Be careful not to use this on too many images at once (making
-    many HTTP requests can be slow).
+    Checks that the url returns an HTTP 200 status code, meaning "ok".
+    Be careful not to use this on too many images at once; making
+    many HTTP requests can be slow.
 
     """
     url = get_devstar_image_url(experiment, well)
@@ -46,6 +61,65 @@ def get_devstar_image_url_if_exists(experiment, well):
         return url
     else:
         return None
+
+
+@register.simple_tag
+def get_thumbnail_td(experiment, library_well):
+    return get_image_td(experiment, library_well, is_thumbnail=True)
+
+
+@register.simple_tag
+def get_devstar_image_td(experiment, library_well):
+    return get_image_td(experiment, library_well, is_devstar=True)
+
+
+@register.simple_tag
+def get_image_td(experiment, library_well, is_thumbnail=False,
+                 is_devstar=False):
+    """Get a td element with an experiment image and some simple information.
+
+    This is used when printing the wells of an entire plate as a table
+    (e.g with 8 rows and 12 columns for a normal 96-well plate, or 96 rows
+    and 1 column for "vertical view" of a 96-well plate, etc).
+
+    The top bar contains minimal info to identify the well (well and tile).
+
+    The lower caption contains the clone name.
+
+    Clicking the image links to more details about that experiment well.
+
+    """
+
+    well = library_well.well
+    experiment_url = reverse('experiment_well_url', args=[experiment.id, well])
+
+    if is_thumbnail:
+        image_url = get_thumbnail_url(experiment, well)
+    elif is_devstar:
+        image_url = get_devstar_image_url(experiment, well)
+    else:
+        image_url = get_image_url(experiment, well)
+
+    if not library_well.intended_clone:
+        td_style = 'class="empty-well"'
+    else:
+        td_style = ''
+
+    return '''
+        <td {0}>
+          <div class="well-title">
+            {1}
+            <span class="parenthetical">({2})</span>
+          </div>
+
+          <a class="image-frame" href="{3}">
+            <img src="{4}">
+          </a>
+
+          <div class="well-caption">{5}</div>
+        </td>
+    '''.format(td_style, well, library_well.get_tile(), experiment_url,
+               image_url, library_well.intended_clone)
 
 
 def get_image_frame(experiment, well):
