ed5dd792c6f2edaf53e0c0d77aee31377691ef4f
Katherine Erickson <katherine.erickson@gmail.com>
2015-10-13 17:41:04 -0400

    Finish working draft of script to migrate mapping information

--- dbmigration/management/commands/import_mapping_data.py
+++ dbmigration/management/commands/import_mapping_data.py
@@ -1,14 +1,14 @@
 import MySQLdb
 
-from django.core.management.base import BaseCommand
+from django.core.exceptions import ObjectDoesNotExist
+from django.core.management.base import BaseCommand, CommandError
 
-from clones.models import Clone
+from clones.models import Clone, Gene, CloneTarget
 from eegi.local_settings import MAPPING_DATABASE
+from utils.db import get_field_dictionary
 from utils.scripting import require_db_write_acknowledgement
 
-HELP = '''
-Import clone data from Firoz's RNAiCloneMapper database.
-'''
+HELP = '''Import clone data from Firoz's RNAiCloneMapper database.'''
 
 
 class Command(BaseCommand):
@@ -21,52 +21,196 @@ class Command(BaseCommand):
                                      user=MAPPING_DATABASE['USER'],
                                      passwd=MAPPING_DATABASE['PASSWORD'],
                                      db=MAPPING_DATABASE['NAME'])
+
         self.cursor = mapping_db.cursor()
+
+        self.mapping_alias_to_pk = self.get_mapping_alias_to_pk()
+        self.mapping_clones = self.get_mapping_clones()
+        self.mapping_genes = self.get_mapping_genes()
+        self.mapping_targets = self.get_mapping_targets()
+
+        self.number_clones_no_targets = 0
+        self.number_clones_multiple_targets = 0
 
         clones = Clone.objects.all()
 
         for clone in clones:
-            mapping_pk = self.get_mapping_pk(clone)
-            self.add_clone_fields(clone, mapping_pk)
-            self.add_clone_aliases(clone)
-            self.add_clone_mappings(clone)
+            self.process_clone(clone)
 
-    def add_clone_aliases(self, clone):
-        pass
+        self.stdout.write('{} clones with no targets.'
+                          .format(self.number_clones_no_targets))
+        self.stdout.write('{} clones with multiple targets.'
+                          .format(self.number_clones_multiple_targets))
 
-    def add_clone_mappings(self, clone):
-        pass
+    def get_mapping_alias_to_pk(self):
+        """Get a dictionary to translate mapping_alias to mapping_pk."""
+        query = 'SELECT alias, clone_id FROM CloneAlias'
 
-    def add_clone_fields(self, clone, mapping_pk):
-        self.cursor.execute('SELECT id, library, clone_type, '
-                            'forward_primer, reverse_primer '
-                            'FROM Clone '
-                            'WHERE id = %s'.format(mapping_pk))
+        self.cursor.execute(query)
+
         rows = self.cursor.fetchall()
 
-        if len(rows) > 1:
-            self.exit('>1 clone match for mapping clone pk %s'
-                      .format(mapping_pk))
-        elif len(rows) < 1:
-            self.exit('No clone match for mapping clone pk %s'
-                      .format(mapping_pk))
+        mapping_alias_to_pk = {}
 
-        row = rows[0]
-        clone.mapping_db_pk = mapping_pk
-        clone.library = row[1]
-        clone.clone_type = row[2]
-        clone.forward_primer = row[3]
-        clone.reverse_primer = row[4]
-        # clone.save()
+        for row in rows:
+            alias, mapping_pk = row
+            if alias not in mapping_alias_to_pk:
+                mapping_alias_to_pk[alias] = []
 
-    def get_mapping_pk(self, clone):
-        self.cursor.execute('SELECT clone_id FROM CloneAlias '
-                            'WHERE alias = `%s`'.format(clone.pk))
+            if mapping_pk not in mapping_alias_to_pk[alias]:
+                mapping_alias_to_pk[alias].append(mapping_pk)
+
+        return mapping_alias_to_pk
+
+    def get_mapping_clones(self):
+        """Get dictionary of all clones from the mapping database.
+
+        This dictionary is keyed on the clone's pk in the mapping
+        database.
+
+        The value is a dictionary of fieldname:value pairs in the
+        mapping database.
+
+        """
+        fieldnames = ['id', 'library', 'clone_type', 'forward_primer',
+                      'reverse_primer']
+        return get_field_dictionary(self.cursor, 'Clone', fieldnames)
+
+    def get_mapping_genes(self):
+        """Get dictionary of all genes from the mapping database.
+
+        This dictionary is keyed on the gene's pk in the mapping
+        database.
+
+        The value is a dictionary of fieldname:value pairs in the
+        mapping database.
+
+        """
+        fieldnames = ['id', 'cosmid_id', 'locus', 'gene_type']
+        return get_field_dictionary(self.cursor, 'Gene', fieldnames)
+
+    def get_mapping_targets(self):
+        """Get dictionary of all targets from the mapping database.
+
+        This dictionary is keyed on the clone's pk in the mapping database.
+
+        The value is a list. Each item in this list is a dictionary
+        capturing the fieldname:value pairs about one target of this clone.
+
+        """
+        fieldnames = [
+            'clone_id', 'id', 'clone_amplicon_id',
+            'amplicon_evidence', 'amplicon_is_designed',
+            'amplicon_is_unique',
+            'gene_id', 'transcript_isoform',
+            'length_span', 'raw_score', 'unique_raw_score',
+            'relative_score', 'specificity_index', 'unique_chunk_index',
+            'is_on_target', 'is_primary_target'
+        ]
+        fieldnames_as_string = ', '.join(fieldnames)
+        query = 'SELECT {} FROM CloneTarget'.format(fieldnames_as_string)
+        self.cursor.execute(query)
         rows = self.cursor.fetchall()
 
-        if len(rows) > 1:
-            self.exit('>1 alias match for %s'.format(clone.pk))
-        elif len(rows) < 1:
-            self.exit('No alias match for %s'.format(clone.pk))
+        all_targets = {}
+        for row in rows:
+            clone_pk = row[0]
+            if clone_pk not in all_targets:
+                all_targets[clone_pk] = []
 
-        return rows[0]
+            this_target = {}
+            for k, v in zip(fieldnames[1:], row[1:]):
+                this_target[k] = v
+
+            all_targets[clone_pk].append(this_target)
+
+        return all_targets
+
+    def process_clone(self, clone):
+        """Do all processing for this clone, both its info and targets."""
+        if clone.pk == 'L4440':
+            return
+
+        if clone.pk not in self.mapping_alias_to_pk:
+            raise CommandError('No alias match for {}'.format(clone.pk))
+
+        mapping_pks = self.mapping_alias_to_pk[clone.pk]
+
+        if len(mapping_pks) > 1:
+            raise CommandError('>1 alias match for {}'.format(clone.pk))
+
+        clone.mapping_db_pk = mapping_pks[0]
+
+        self.update_clone_info(clone)
+        self.update_clone_targets(clone)
+
+    def update_clone_targets(self, clone):
+        """Do all processing for this clone's targets."""
+        try:
+            mapping_targets = self.mapping_targets[clone.mapping_db_pk]
+        except KeyError:
+            self.stdout.write('Clone {} has no targets'.format(clone))
+            self.number_clones_no_targets += 1
+            return
+
+        if len(mapping_targets) > 1:
+            self.number_clones_multiple_targets += 1
+
+        for target_info in mapping_targets:
+            gene_id = target_info['gene_id']
+
+            try:
+                gene = Gene.objects.get(pk=gene_id)
+            except ObjectDoesNotExist:
+                if gene_id in self.mapping_genes:
+                    gene = Gene(id=gene_id)
+                else:
+                    self.stdout.write('Gene {} from targets table not present '
+                                      'in gene table'.format(gene_id))
+                    continue
+
+            self.update_gene_info(gene)
+
+            target_id = target_info['id']
+
+            try:
+                target = CloneTarget.objects.get(pk=target_id)
+            except ObjectDoesNotExist:
+                target = CloneTarget(id=target_id)
+
+            self.update_target_info(target, clone, gene, target_info)
+
+    def update_clone_info(self, clone):
+        clone_info = self.mapping_clones[clone.mapping_db_pk]
+        clone.library = clone_info['library']
+        clone.clone_type = clone_info['clone_type']
+        clone.forward_primer = clone_info['forward_primer']
+        clone.reverse_primer = clone_info['reverse_primer']
+        clone.save()
+
+    def update_gene_info(self, gene):
+        gene_info = self.mapping_genes[gene.id]
+        gene.cosmid_id = gene_info['cosmid_id']
+        gene.locus = gene_info['locus']
+        if gene.locus == 'NA':
+            gene.locus = ''
+        gene.gene_type = gene_info['gene_type']
+        gene.save()
+
+    def update_target_info(self, target, clone, gene, target_info):
+        target.clone = clone
+        target.gene = gene
+        target.clone_amplicon_id = target_info['clone_amplicon_id']
+        target.amplicon_evidence = target_info['amplicon_evidence']
+        target.amplicon_is_designed = target_info['amplicon_is_designed']
+        target.amplicon_is_unique = target_info['amplicon_is_unique']
+        target.transcript_isoform = target_info['transcript_isoform']
+        target.length_span = target_info['length_span']
+        target.raw_score = target_info['raw_score']
+        target.unique_raw_score = target_info['unique_raw_score']
+        target.relative_score = target_info['relative_score']
+        target.specificity_index = target_info['specificity_index']
+        target.unique_chunk_index = target_info['unique_chunk_index']
+        target.is_on_target = target_info['is_on_target']
+        target.is_primary_target = target_info['is_primary_target']
+        target.save()
---
+++ utils/db.py
@@ -0,0 +1,22 @@
+def get_field_dictionary(cursor, table, fieldnames):
+    """Get a dictionary capturing fields of a table.
+
+    Assumes fieldnames[0] is the primary key of table.
+    The dictionary returned is keyed on this primary key.
+
+    The value of the dictionary returned is a nested dictionary.
+    This nested dictionary is keyed on fieldnames [1:],
+    the values being the corresponding values from the table.
+
+    """
+    fieldnames_as_string = ', '.join(fieldnames)
+    query = 'SELECT {} FROM {}'.format(fieldnames_as_string, table)
+    cursor.execute(query)
+    rows = cursor.fetchall()
+    d = {}
+    for row in rows:
+        pk = row[0]
+        d[pk] = {}
+        for k, v in zip(fieldnames[1:], row[1:]):
+            d[pk][k] = v
+    return d
