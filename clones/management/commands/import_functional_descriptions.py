6f8b4c3e2d56493e6f7132988f9900c966f2c1dd
Katherine Erickson <katherine.erickson@gmail.com>
2015-10-14 13:14:36 -0400

    More work on script to import functional descriptions

--- clones/management/commands/import_functional_descriptions.py
+++ clones/management/commands/import_functional_descriptions.py
@@ -30,26 +30,34 @@ class Command(BaseCommand):
             info = descriptions[gene.id]
 
             # Sanity checks
-            molecular_name = info['molecular_name']
-            public_name = info['public_name']
+            num_mismatches = 0
 
-            if (not molecular_name.startswith(gene.cosmid_id) and
-                    gene.cosmid_id != public_name):
-                self.stdout.write('Cosmid mismatch for {}: '
-                                  'Firoz says {}, WormBase says {}'
-                                  .format(gene, gene.cosmid_id,
-                                          molecular_name))
+            # Does WormBase molecular_name align with Firoz's database?
+            wb_molecular = info['molecular_name']
+            firoz_cosmid = gene.cosmid_id
+            if (not wb_molecular.startswith(firoz_cosmid)):
+                num_mismatches += 1
+                self.stdout.write('Molecular/cosmid mismatch for {}: '
+                                  'WormBase says {}, Firoz says {}'
+                                  .format(gene, wb_molecular, firoz_cosmid))
 
-            if (public_name != gene.locus and
-                    not molecular_name.startswith(public_name) and
-                    not public_name.startswith(gene.cosmid_id)):
-                self.stdout.write('Locus mismatch for {}: '
-                                  'Firoz says {}, WormBase says {}'
-                                  .format(gene, gene.locus, public_name))
+            # Does WormBase public_name align with Firoz's database?
+            wb_public = info['public_name']
+            firoz_locus = gene.locus
+            if (wb_public != firoz_locus and
+                    not (firoz_locus == '' and wb_public == firoz_cosmid)):
+                num_mismatches += 1
+                self.stdout.write('Public/locus mismatch for {}: '
+                                  'WormBase says {}, Firoz says {}'
+                                  .format(gene, wb_public, firoz_locus))
 
             description = info['concise_description']
             gene.functional_description = description
             gene.save()
+
+        if num_mismatches:
+            self.stdout.write('Total number mismatches: {}'
+                              .format(num_mismatches))
 
     def parse_wormbase_file(self, f):
         # Skip header
