d35c90cc9a19ce71ddf3b27b720ccc1eb4ead14f
Katherine Erickson <katherine.erickson@gmail.com>
2015-10-22 11:40:40 -0400

    Clean up code for knockdown forms.
    Move logic from views into form clean() methods.

--- experiments/forms.py
+++ experiments/forms.py
@@ -1,12 +1,8 @@
 from django import forms
-from experiments.models import Experiment
 
-SCREEN_CHOICES = [('SUP', 'suppressor'), ('ENH', 'enhancer')]
-MUTANT_QUERY_LABEL = 'Mutant query'
-RNAI_QUERY_LABEL = 'RNAi query'
-MUTANT_QUERY_HELP_TEXT = 'gene, allele, or worm strain name'
-RNAI_QUERY_HELP_TEXT = ('gene target (WormBase id, cosmid id, or locus) or '
-                        'clone name')
+from clones.helpers.queries import get_clones
+from experiments.models import Experiment
+from worms.helpers.queries import get_worm_and_temperature
 
 
 class BlankNullBooleanSelect(forms.NullBooleanSelect):
@@ -77,41 +73,97 @@ class ExperimentFilterForm(forms.Form):
         required=False)
 
 
+class MutantQueryField(forms.CharField):
+    def __init__(self):
+        super(MutantQueryField, self).__init__(
+            label='Mutant query',
+            help_text='gene, allele, or worm strain name')
+
+
+class RNAiQueryField(forms.CharField):
+    def __init__(self):
+        super(RNAiQueryField, self).__init__(
+            label='RNAi query',
+            help_text='gene target (WormBase id, cosmid id, or locus)'
+                      ' or clone name')
+
+
+class ScreenChoiceField(forms.ChoiceField):
+    def __init__(self):
+        super(ScreenChoiceField, self).__init__(
+            choices=[('SUP', 'suppressor'), ('ENH', 'enhancer')],
+            widget=forms.RadioSelect)
+
+
+def _clean_mutant_query_and_screen(form, cleaned_data):
+    mutant_query = cleaned_data.get('mutant_query')
+    screen = cleaned_data.get('screen')
+
+    if mutant_query == 'N2':
+        form.add_error('mutant_query', 'Mutant query cannot be N2')
+
+    elif mutant_query:
+        worm_and_temp = get_worm_and_temperature(mutant_query, screen)
+        if worm_and_temp:
+            cleaned_data['worm'] = worm_and_temp[0]
+            cleaned_data['temperature'] = worm_and_temp[1]
+        else:
+            form.add_error('mutant_query', 'No mutant match')
+
+    return cleaned_data
+
+
+def _clean_rnai_query(form, cleaned_data):
+    rnai_query = cleaned_data.get('rnai_query')
+
+    if rnai_query == 'L4440':
+        form.add_error('rnai_query', 'RNAi query cannot be L4440')
+
+    clones = get_clones(rnai_query)
+    if clones:
+        cleaned_data['clones'] = clones
+    else:
+        form.add_error('rnai_query', 'No RNAi match')
+
+
 class DoubleKnockdownForm(forms.Form):
     """Form for finding a double knockdown."""
-    mutant_query = forms.CharField(label=MUTANT_QUERY_LABEL,
-                                   help_text=MUTANT_QUERY_HELP_TEXT)
+    mutant_query = MutantQueryField()
+    rnai_query = RNAiQueryField()
+    screen = ScreenChoiceField()
 
-    rnai_query = forms.CharField(label=RNAI_QUERY_LABEL,
-                                 help_text=RNAI_QUERY_HELP_TEXT)
-
-    screen = forms.ChoiceField(choices=SCREEN_CHOICES,
-                               widget=forms.RadioSelect)
+    def clean(self):
+        cleaned_data = super(DoubleKnockdownForm, self).clean()
+        cleaned_data = _clean_mutant_query_and_screen(self, cleaned_data)
+        cleaned_data = _clean_rnai_query(self, cleaned_data)
+        return cleaned_data
 
 
 class MutantKnockdownForm(forms.Form):
     """Form for finding a mutant worm with the control bacteria."""
-    mutant_query = forms.CharField(label=MUTANT_QUERY_LABEL,
-                                   help_text=MUTANT_QUERY_HELP_TEXT)
+    mutant_query = MutantQueryField()
+    screen = ScreenChoiceField()
 
-    screen = forms.ChoiceField(label="Screen temperature",
-                               choices=SCREEN_CHOICES,
-                               widget=forms.RadioSelect)
+    def clean(self):
+        cleaned_data = super(MutantKnockdownForm, self).clean()
+        cleaned_data = _clean_mutant_query_and_screen(self, cleaned_data)
+        return cleaned_data
 
 
 class RNAiKnockdownForm(forms.Form):
     """Form for finding wildtype worms tested with a single RNAi clone."""
-    rnai_query = forms.CharField(label=RNAI_QUERY_LABEL,
-                                 help_text=RNAI_QUERY_HELP_TEXT)
-
+    rnai_query = RNAiQueryField()
     temperature = forms.DecimalField(required=False,
                                      label='Temperature',
                                      help_text='optional')
 
+    def clean(self):
+        cleaned_data = super(RNAiKnockdownForm, self).clean()
+        cleaned_data = _clean_rnai_query(self, cleaned_data)
+        return cleaned_data
+
 
 class SecondaryScoresForm(forms.Form):
     """Form for getting all secondary scores for a strain."""
-    query = forms.CharField(label=MUTANT_QUERY_LABEL,
-                            help_text=MUTANT_QUERY_HELP_TEXT)
-    screen = forms.ChoiceField(choices=SCREEN_CHOICES,
-                               widget=forms.RadioSelect)
+    query = MutantQueryField()
+    screen = ScreenChoiceField()
--- experiments/views/views_knockdown_search.py
+++ experiments/views/views_knockdown_search.py
@@ -1,64 +1,28 @@
 from django.shortcuts import render, redirect
 
-from clones.helpers.queries import get_clones
 from experiments.forms import (
     DoubleKnockdownForm, MutantKnockdownForm, RNAiKnockdownForm)
 from experiments.views import (
     double_knockdown, mutant_knockdown, rnai_knockdown)
-from worms.helpers.queries import get_worm_and_temperature
-
-
-def get_clone_ids(query):
-    """Get a comma-separated string listing the clones that match a query term.
-
-    The clone could match the query term either in the clone's name, or
-    in the wormbase id, cosmid id, or locus of any of its targets.
-
-    """
-    clones = get_clones(query)
-    if not clones:
-        return None
-    return ','.join(clone.id for clone in clones)
 
 
 def double_knockdown_search(request):
     """Render the page to search for a double knockdown."""
-    error = ''
     if request.method == 'POST':
         form = DoubleKnockdownForm(request.POST)
+
         if form.is_valid():
-            try:
-                data = form.cleaned_data
-                mutant_query = data['mutant_query']
-                rnai_query = data['rnai_query']
-                screen = data['screen']
-
-                if mutant_query == 'N2':
-                    raise Exception('mutant query cannot be N2')
-
-                if rnai_query == 'L4440':
-                    raise Exception('rnai query cannot be L4440')
-
-                worm_and_temp = get_worm_and_temperature(mutant_query, screen)
-                if not worm_and_temp:
-                    raise Exception('No mutant match')
-                worm, temperature = worm_and_temp
-
-                clones = get_clone_ids(rnai_query)
-                if not clones:
-                    raise Exception('No RNAi match')
-
-                return redirect(double_knockdown, worm, clones, temperature)
-
-            except Exception as e:
-                error = e.message
+            data = form.cleaned_data
+            worm = data['worm']
+            temperature = data['temperature']
+            clones = ','.join(clone.id for clone in data['clones'])
+            return redirect(double_knockdown, worm, clones, temperature)
 
     else:
         form = DoubleKnockdownForm(initial={'screen': 'SUP'})
 
     context = {
         'form': form,
-        'error': error,
     }
 
     return render(request, 'double_knockdown_search.html', context)
@@ -66,57 +30,43 @@ def double_knockdown_search(request):
 
 def single_knockdown_search(request):
     """Render the page to search for a single knockdown."""
-    error = ''
+    empty_rnai_form = RNAiKnockdownForm(prefix='rnai')
+    empty_mutant_form = MutantKnockdownForm(prefix='mutant',
+                                            initial={'screen': 'SUP'})
+
     if request.method == 'POST' and 'rnai' in request.POST:
-        mutant_form = MutantKnockdownForm(prefix='mutant',
-                                          initial={'screen': 'SUP'})
         rnai_form = RNAiKnockdownForm(request.POST, prefix='rnai')
+
         if rnai_form.is_valid():
-            try:
-                data = rnai_form.cleaned_data
-                rnai_query = data['rnai_query']
-                temperature = data['temperature']
+            data = rnai_form.cleaned_data
+            temperature = data['temperature']
+            clones = ','.join(clone.id for clone in data['clones'])
 
-                clones = get_clone_ids(rnai_query)
-                if not clones:
-                    raise Exception('No RNAi match')
+            if temperature:
+                return redirect(rnai_knockdown, clones, temperature)
+            else:
+                return redirect(rnai_knockdown, clones)
 
-                if temperature:
-                    return redirect(rnai_knockdown, clones, temperature)
-                else:
-                    return redirect(rnai_knockdown, clones)
-
-            except Exception as e:
-                error = e.message
+        mutant_form = empty_mutant_form
 
     elif request.method == 'POST' and 'mutant' in request.POST:
-        rnai_form = RNAiKnockdownForm(prefix='rnai')
         mutant_form = MutantKnockdownForm(request.POST, prefix='mutant')
+
         if mutant_form.is_valid():
-            try:
-                data = mutant_form.cleaned_data
-                mutant_query = data['mutant_query']
-                screen = data['screen']
+            data = mutant_form.cleaned_data
+            worm = data['worm']
+            temperature = data['temperature']
+            return redirect(mutant_knockdown, worm, temperature)
 
-                worm_and_temp = get_worm_and_temperature(mutant_query, screen)
-                if not worm_and_temp:
-                    raise Exception('No mutant match')
-                worm, temperature = worm_and_temp
-
-                return redirect(mutant_knockdown, worm, temperature)
-
-            except Exception as e:
-                error = e.message
+        rnai_form = empty_rnai_form
 
     else:
-        rnai_form = RNAiKnockdownForm(prefix='rnai')
-        mutant_form = MutantKnockdownForm(prefix='mutant',
-                                          initial={'screen': 'SUP'})
+        mutant_form = empty_mutant_form
+        rnai_form = empty_rnai_form
 
     context = {
         'rnai_form': rnai_form,
         'mutant_form': mutant_form,
-        'error': error,
     }
 
     return render(request, 'single_knockdown_search.html', context)
