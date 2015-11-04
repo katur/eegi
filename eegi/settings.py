bcacf23af8f905cdca412c7fc75b19c99c87fc04
Katherine Erickson <katherine.erickson@gmail.com>
2015-11-04 17:44:34 -0500

    Add test to refactor branch

--- eegi/settings.py
+++ eegi/settings.py
@@ -10,6 +10,8 @@ from local_settings import (DEBUG, DATABASE,
                             LOCKDOWN_PASSWORDS, SECRET_KEY)
 
 
+### TEST
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
 import os
