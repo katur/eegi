2733d26a9b1dee4ebee847b2badb744390bdf21d
Katherine Erickson <katherine.erickson@gmail.com>
2015-11-11 11:20:21 -0500

    Clean up import_func_descriptions script

--- clones/management/commands/import_functional_descriptions.py
+++ clones/management/commands/import_functional_descriptions.py
@@ -1,3 +1,16 @@
+"""Command to add WormBase gene functional descriptions.
+
+The WormBase file can be found at:
+
+ftp://ftp.wormbase.org/pub/wormbase/releases/WSX/species/c_elegans/
+PRJNA13758/annotation/c_elegans.PRJNA13758.WSX.functional_descriptions.txt.gz
+
+Where WSX should be replaced with the desired WormBase version.
+
+As of November 2015, Firoz's mapping database uses WS240, so Katherine
+also used WS240 for the functional descriptions.
+
+"""
 import argparse
 import csv
 
@@ -13,14 +26,15 @@ class Command(BaseCommand):
     help = HELP
 
     def add_arguments(self, parser):
-        parser.add_argument('file', type=argparse.FileType('r'))
+        parser.add_argument('file', type=argparse.FileType('r'),
+                            help=('WormBase functional descriptions file'))
 
     def handle(self, **options):
         f = options['file']
 
         require_db_write_acknowledgement()
 
-        descriptions = self.parse_wormbase_file(f)
+        descriptions = parse_wormbase_file(f)
 
         genes = Gene.objects.all()
 
@@ -60,23 +74,24 @@ class Command(BaseCommand):
             self.stderr.write('Total number mismatches: {}'
                               .format(num_mismatches))
 
-    def parse_wormbase_file(self, f):
-        # Skip header
-        while True:
-            row = next(f)
-            if row[0] != '#':
-                break
 
-        fieldnames = row
-        fieldnames = fieldnames.split()
+def parse_wormbase_file(f):
+    # Skip header
+    while True:
+        row = next(f)
+        if row[0] != '#':
+            break
 
-        reader = csv.reader(f, delimiter='\t')
+    fieldnames = row
+    fieldnames = fieldnames.split()
 
-        d = {}
-        for row in reader:
-            gene_id = row[0]
-            d[gene_id] = {}
-            for k, v in zip(fieldnames[1:], row[1:]):
-                d[gene_id][k] = v
+    reader = csv.reader(f, delimiter='\t')
 
-        return d
+    d = {}
+    for row in reader:
+        gene_id = row[0]
+        d[gene_id] = {}
+        for k, v in zip(fieldnames[1:], row[1:]):
+            d[gene_id][k] = v
+
+    return d
