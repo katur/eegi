4e9d12c919ece13995342f94f44ab983fade5347
Katherine Erickson <katherine.erickson@gmail.com>
2015-11-13 16:05:27 -0500

    Refactor experiment models.
    Move most methods from Experiment (plate) to ExperimentWell.
    
    Move ManualScore constants into ManualScore class.
    
    Fix up a bunch of helpers per the refactor.

--- experiments/models.py
+++ experiments/models.py
@@ -2,31 +2,11 @@ from __future__ import division
 
 from django.contrib.auth.models import User
 from django.db import models
-from django.db.models import Q
 
 from eegi.settings import IMG_PATH, THUMBNAIL_PATH, DEVSTAR_PATH
-from library.models import LibraryWell
+from library.models import LibraryPlate, LibraryWell
 from utils.well_tile_conversion import well_to_tile
 from worms.models import WormStrain
-
-
-STRONG = 'Strong'
-MEDIUM = 'Medium'
-WEAK = 'Weak'
-OTHER = 'Other'
-NEGATIVE = 'Negative'
-UNSCORED = 'Unscored'
-
-SCORE_WEIGHTS = {
-    STRONG: 3,
-    MEDIUM: 2,
-    WEAK: 1,
-    NEGATIVE: 0,
-    OTHER: 0,
-}
-
-RELEVANCE_PER_REPLICATE = (OTHER, NEGATIVE, WEAK, MEDIUM, STRONG)
-RELEVANCE_ACROSS_REPLICATES = (NEGATIVE, OTHER, UNSCORED, WEAK, MEDIUM, STRONG)
 
 
 class ExperimentPlate(models.Model):
@@ -50,84 +30,19 @@ class ExperimentPlate(models.Model):
     def __unicode__(self):
         return str(self.id)
 
-    def get_image_url(self, well, mode=None):
-        """Get the image url for this experiment, at position well.
+    def get_worm_strains(self):
+        worm_pks = (self.experimentwell_set.order_by('worm_strain')
+                    .values('worm_strain').distinct())
 
-        well should be a string.
+        return WormStrain.objects.filter(pk__in=worm_pks)
 
-        Optionally supply mode='thumbnail' or mode='devstar'.
+    def get_library_plates(self):
+        library_plate_pks = (self.experimentwell_set
+                             .order_by('library_well__library_plate')
+                             .values('library_well__library_plate')
+                             .distinct())
 
-        """
-        tile = well_to_tile(well)
-        if mode == 'thumbnail':
-            url = '/'.join((THUMBNAIL_PATH, str(self.id), tile))
-            url += '.jpg'
-        elif mode == 'devstar':
-            url = '/'.join((DEVSTAR_PATH, str(self.id), tile))
-            url += 'res.png'
-        else:
-            url = '/'.join((IMG_PATH, str(self.id), tile))
-            url += '.bmp'
-        return url
-
-    def get_manual_scores(self, well=None):
-        """Get all Manual scores for this experiment.
-
-        Defaults to returning all scores for the plate.
-        Optionally specify a well to limit to the scores for a
-        particular well.
-
-        """
-        if well:
-            scores = (ManualScore.objects
-                      .filter(Q(experiment=self), Q(well=well))
-                      .order_by('scorer', 'timestamp', 'score_code'))
-        else:
-            scores = ManualScore.objects.filter(experiment=self)
-
-        return scores
-
-    def get_devstar_scores(self, well=None):
-        """Get all DevStaR scores for this experiment.
-
-        Defaults to returning all scores for the plate.
-        Optionally specify a well to limit to the scores for a
-        particular well.
-
-        """
-        if well:
-            scores = (DevstarScore.objects
-                      .filter(Q(experiment=self), Q(well=well)))
-        else:
-            scores = DevstarScore.objects.filter(experiment=self)
-
-        return scores
-
-    def is_manually_scored(self, well=None):
-        """Determine whether an experiment was manually scored.
-
-        Defaults to checking if any well in the plate was scored.
-        Optionally specify a well to get whether a particular well
-        was scored.
-
-        """
-        if self.get_manual_scores(well=well):
-            return True
-        else:
-            return False
-
-    def is_devstar_scored(self, well=None):
-        """Determine whether an experiment was scored by DevStaR.
-
-        Defaults to checking if any well in the plate was scored.
-        Optionally specify a well to get whether a particular well
-        was scored.
-
-        """
-        if self.get_devstar_scores(well=well):
-            return True
-        else:
-            return False
+        return LibraryPlate.objects.filter(pk__in=library_plate_pks)
 
 
 class ExperimentWell(models.Model):
@@ -146,6 +61,81 @@ class ExperimentWell(models.Model):
 
     def __unicode__(self):
         return str(self.id)
+
+    def has_control_worm(self):
+        return self.worm_strain.is_control()
+
+    def has_control_clone(self):
+        return self.library_well.intended_clone.is_control()
+
+    def is_control(self):
+        return self.has_control_worm() or self.has_control_clone()
+
+    def date(self):
+        return self.experiment_plate.date
+
+    def screen_stage(self):
+        return self.experiment_plate.screen_stage
+
+    def temperature(self):
+        return self.experiment_plate.temperature
+
+    def intended_clone(self):
+        return self.library_well.intended_clone
+
+    # TODO: These three methods are repeated in ExperimentWell and
+    # LibraryWell; consider making a superclass or mixin
+    def get_row(self):
+        return self.well[0]
+
+    def get_column(self):
+        return int(self.well[1:])
+
+    def get_tile(self):
+        return well_to_tile(self.well)
+
+    def get_image_url(self, mode=None):
+        """Get the image url for this experiment well.
+
+        Optionally supply mode='thumbnail' or mode='devstar'.
+
+        """
+        tile = well_to_tile(self.well)
+        if mode == 'thumbnail':
+            url = '/'.join((THUMBNAIL_PATH,
+                            str(self.experiment_plate_id), tile))
+            url += '.jpg'
+        elif mode == 'devstar':
+            url = '/'.join((DEVSTAR_PATH,
+                            str(self.experiment_plate_id), tile))
+            url += 'res.png'
+        else:
+            url = '/'.join((IMG_PATH,
+                            str(self.experiment_plate_id), tile))
+            url += '.bmp'
+        return url
+
+    def get_manual_scores(self):
+        """Get all manual scores for this experiment well."""
+        return self.manualscore_set.all()
+
+    def is_manually_scored(self):
+        """Check if an experiment was manually scored."""
+        if self.get_manual_scores():
+            return True
+        else:
+            return False
+
+    def get_devstar_scores(self):
+        """Get all DevStaR scores for this experiment."""
+        return self.devstarscore_set.all()
+
+    def is_devstar_scored(self):
+        """Check if an experiment was scored by DevStaR."""
+        if self.get_devstar_scores():
+            return True
+        else:
+            return False
 
 
 class ManualScoreCode(models.Model):
@@ -198,16 +188,37 @@ class ManualScore(models.Model):
     scorer = models.ForeignKey(User)
     timestamp = models.DateTimeField()
 
+    STRONG = 'Strong'
+    MEDIUM = 'Medium'
+    WEAK = 'Weak'
+    OTHER = 'Other'
+    NEGATIVE = 'Negative'
+    UNSCORED = 'Unscored'
+
+    WEIGHTS = {
+        STRONG: 3,
+        MEDIUM: 2,
+        WEAK: 1,
+        NEGATIVE: 0,
+        OTHER: 0,
+    }
+
+    RELEVANCE_PER_REPLICATE = (OTHER, NEGATIVE, WEAK, MEDIUM, STRONG)
+    RELEVANCE_ACROSS_REPLICATES = (NEGATIVE, OTHER, UNSCORED, WEAK, MEDIUM,
+                                   STRONG)
+
     class Meta:
         db_table = 'ManualScore'
+        ordering = ['scorer', 'timestamp', 'score_code']
 
     def __unicode__(self):
-        return ('{}@{} scored {} by {}'
-                .format(self.experiment_id, self.well,
-                        self.score_code, self.scorer))
+        return ('{} scored {} by {}'
+                .format(self.experiment_well_id, self.score_code,
+                        self.scorer))
 
     def get_short_description(self):
-        return '{} ({})'.format(self.score_code, self.scorer.get_short_name())
+        return '{} ({})'.format(self.score_code,
+                                self.scorer.get_short_name())
 
     def is_strong(self):
         return self.score_code.is_strong()
@@ -226,15 +237,15 @@ class ManualScore(models.Model):
 
     def get_category(self):
         if self.is_strong():
-            return STRONG
+            return ManualScore.STRONG
         elif self.is_medium():
-            return MEDIUM
+            return ManualScore.MEDIUM
         elif self.is_weak():
-            return WEAK
+            return ManualScore.WEAK
         elif self.is_negative():
-            return NEGATIVE
+            return ManualScore.NEGATIVE
         elif self.is_other():
-            return OTHER
+            return ManualScore.OTHER
 
     def get_weight(self):
         """Returns a relevance weight for this score.
@@ -246,13 +257,14 @@ class ManualScore(models.Model):
         problem or a phenotype unrelated to sup/enh).
 
         """
-        return SCORE_WEIGHTS[self.get_category()]
+        return ManualScore.WEIGHTS[self.get_category()]
 
     def get_relevance_per_replicate(self):
-        return RELEVANCE_PER_REPLICATE.index(self.get_category())
+        return ManualScore.RELEVANCE_PER_REPLICATE.index(self.get_category())
 
     def get_relevance_across_replicates(self):
-        return RELEVANCE_ACROSS_REPLICATES.index(self.get_category())
+        return ManualScore.RELEVANCE_ACROSS_REPLICATES.index(
+            self.get_category())
 
 
 class DevstarScore(models.Model):
@@ -302,8 +314,7 @@ class DevstarScore(models.Model):
         db_table = 'DevstarScore'
 
     def __unicode__(self):
-        return ('{}@{} DevStaR score'
-                .format(self.experiment_id, self.well))
+        return ('{} DevStaR score'.format(self.experiment_well))
 
     def clean(self):
         # Set the fields calculated from the DevStaR fields (resets if already
