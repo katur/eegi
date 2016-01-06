b3b118dd2462c5f258faa4bdb8d982ebadce09fb
Katherine Erickson <katherine.erickson@gmail.com>
2016-01-06 17:31:37 -0500

    Dashed line for Empty nullbooleanselect

--- utils/forms.py
+++ utils/forms.py
@@ -5,7 +5,7 @@ from django import forms
 
 class BlankNullBooleanSelect(forms.NullBooleanSelect):
     def __init__(self, attrs=None):
-        choices = ((u'1', u''),
+        choices = ((u'1', u'---'),
                    (u'3', u'No'),
                    (u'2', u'Yes'))
         forms.Select.__init__(self, attrs, choices)
