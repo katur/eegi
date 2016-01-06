ce3b9c75790f95ee5ecde2ce1c90e5ef4e7b46f7
Katherine Erickson <katherine.erickson@gmail.com>
2016-01-06 16:32:17 -0500

    Create utility for connecting to google spreadsheets

---
+++ utils/google.py
@@ -0,0 +1,16 @@
+import gspread
+import json
+from oauth2client.client import SignedJwtAssertionCredentials
+
+from eegi.settings import GOOGLE_API_KEY
+
+
+def connect_to_google_spreadsheets():
+    """Connect to Google Spreadsheets with API key."""
+    json_key = json.load(open(GOOGLE_API_KEY))
+    scope = ['https://spreadsheets.google.com/feeds']
+    credentials = SignedJwtAssertionCredentials(
+            json_key['client_email'], json_key['private_key'].encode(),
+            scope)
+    gc = gspread.authorize(credentials)
+    return gc
