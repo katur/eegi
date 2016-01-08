d5f397e504aa0222cc6e8bdf741acab031f0cdcf
Katherine Erickson <katherine.erickson@gmail.com>
2016-01-08 18:42:02 -0500

    Fix login/logout and website patterns

--- eegi/urls.py
+++ eegi/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
-from django.contrib import admin
+from django.contrib import admin, auth
+from django.contrib.auth.views import login, logout
 
 
 admin.autodiscover()
@@ -11,5 +12,9 @@ urlpatterns = [
     url(r'', include('clones.urls')),
     url(r'', include('library.urls')),
     url(r'', include('experiments.urls')),
+    url(r'^login/$', auth.views.login,
+        {'template_name': 'login.html'}, name='login_url'),
+    url(r'^logout/$', auth.views.logout,
+        {'next_page': '/'}, name='logout_url'),
     url(r'^admin/', admin.site.urls),
 ]
--- website/urls.py
+++ website/urls.py
@@ -1,16 +1,8 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+
+from website import views
 
 
-urlpatterns = patterns(
-    'website.views',
-    url(r'^$', 'home', name='home_url'),
-)
-
-
-urlpatterns += patterns(
-    '',
-    url(r'^login/$', 'django.contrib.auth.views.login',
-        {'template_name': 'login.html'}, name='login_url'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout',
-        {'next_page': '/'}, name='logout_url'),
-)
+urlpatterns = [
+    url(r'^$', views.home, name='home_url'),
+]
