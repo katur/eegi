0e36ca1a2656da8a11bcec0b9abbed64963aa7a1
Katherine Erickson <katherine.erickson@gmail.com>
2016-01-11 12:06:56 -0500

    Add trailing slash to URLs (to take advantage of Django built-in APPEND_SLASH setting)

--- clones/urls.py
+++ clones/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'clones.views',
-    url(r'^clones$', 'clones', name='clones_url'),
-    url(r'^clone/([^/]*)$', 'clone', name='clone_url'),
+    url(r'^clones/$', 'clones', name='clones_url'),
+    url(r'^clone/([^/]*)/$', 'clone', name='clone_url'),
 )
--- worms/urls.py
+++ worms/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns(
     'worms.views',
-    url(r'^worm-strains$', 'worms', name='worms_url'),
+    url(r'^worm-strains/$', 'worms', name='worms_url'),
 )
