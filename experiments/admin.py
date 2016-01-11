f9e296c388c9eb19391019c396abb213cb8dc4c1
Katherine Erickson <katherine.erickson@gmail.com>
2016-01-11 13:58:19 -0500

    Admin cleanup

--- experiments/admin.py
+++ experiments/admin.py
@@ -1,27 +1,26 @@
 from django.contrib import admin
 
 from experiments.models import (Experiment, ExperimentPlate,
-                                ManualScoreCode, ManualScore, DevstarScore)
+                                ManualScoreCode, ManualScore)
 from website.admin import ReadOnlyAdmin
 
 
 class ExperimentAdmin(admin.ModelAdmin):
     list_display = ('id',)
 
-    list_filter = ('worm_strain', 'is_junk',)
-
     search_fields = ('id',)
 
-    raw_id_fields = ('plate', 'library_stock',)
-
     readonly_fields = ('id', 'plate', 'well',)
+
+    # Do not load dropdown with all library_stocks, because there are so many
+    raw_id_fields = ('library_stock',)
 
     fields = ('id', 'plate', 'well', 'worm_strain', 'library_stock',
               'is_junk', 'comment',)
 
 
 class ExperimentPlateAdmin(admin.ModelAdmin):
-    list_display = ('id', 'screen_stage', 'temperature', 'date',)
+    list_display = ('id',)
 
     search_fields = ('id',)
 
@@ -46,17 +45,7 @@ class ManualScoreAdmin(admin.ModelAdmin):
     raw_id_fields = ('experiment',)
 
 
-class DevstarScoreAdmin(ReadOnlyAdmin):
-    list_display = ('experiment', 'area_adult', 'area_larva', 'area_embryo',
-                    'count_adult', 'count_larva',)
-
-    search_fields = ('experiment__id',)
-
-    raw_id_fields = ('experiment',)
-
-
 admin.site.register(Experiment, ExperimentAdmin)
 admin.site.register(ExperimentPlate, ExperimentPlateAdmin)
 admin.site.register(ManualScoreCode, ManualScoreCodeAdmin)
 admin.site.register(ManualScore, ManualScoreAdmin)
-admin.site.register(DevstarScore, DevstarScoreAdmin)
