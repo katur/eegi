885dde056508ec30d6fa60c11fb6160f6a7725c5
Katherine Erickson <katherine.erickson@gmail.com>
2016-01-20 14:10:21 -0500

    Stop using url patterns() in experiments app

--- experiments/urls.py
+++ experiments/urls.py
@@ -1,45 +1,45 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+
+from . import views
 
 
-urlpatterns = patterns(
-    'experiments.views',
-    url(r'^experiment/(\d+_[A-H]\d\d)/$', 'experiment_well',
+urlpatterns = [
+    url(r'^experiment/(\d+_[A-H]\d\d)/$', views.experiment_well,
         name='experiment_well_url'),
-    url(r'^experiment/(\d+)/$', 'experiment_plate',
+    url(r'^experiment/(\d+)/$', views.experiment_plate,
         name='experiment_plate_url'),
     url(r'^vertical-experiment-plates/([\d,]+)/$',
-        'vertical_experiment_plates',
+        views.vertical_experiment_plates,
         name='vertical_experiment_plates_url'),
 
-    url(r'^find-experiment-wells/$', 'find_experiment_wells',
+    url(r'^find-experiment-wells/$', views.find_experiment_wells,
         name='find_experiment_wells_url'),
-    url(r'^find-experiment-plates/$', 'find_experiment_plates',
+    url(r'^find-experiment-plates/$', views.find_experiment_plates,
         name='find_experiment_plates_url'),
 
-    url(r'^new-experiment-plate/$', 'new_experiment_plate_and_wells',
+    url(r'^new-experiment-plate/$', views.new_experiment_plate_and_wells,
         name='new_experiment_plate_and_wells_url'),
 
-    url(r'^single-knockdown/$', 'single_knockdown_search',
+    url(r'^single-knockdown/$', views.single_knockdown_search,
         name='single_knockdown_search_url'),
-    url(r'^rnai-knockdown/([^/]+)/$', 'rnai_knockdown',
+    url(r'^rnai-knockdown/([^/]+)/$', views.rnai_knockdown,
         name='rnai_knockdown_url'),
-    url(r'^rnai-knockdown/([^/]+)/([^/]+)/$', 'rnai_knockdown',
+    url(r'^rnai-knockdown/([^/]+)/([^/]+)/$', views.rnai_knockdown,
         name='rnai_knockdown_url'),
-    url(r'^mutant-knockdown/([^/]+)/([^/]+)/$', 'mutant_knockdown',
+    url(r'^mutant-knockdown/([^/]+)/([^/]+)/$', views.mutant_knockdown,
         name='mutant_knockdown_url'),
-    url(r'^double-knockdown/$', 'double_knockdown_search',
+    url(r'^double-knockdown/$', views.double_knockdown_search,
         name='double_knockdown_search_url'),
     url(r'^double-knockdown/([^/]+)/([^/]+)/([^/]+)/$',
-        'double_knockdown',
-        name='double_knockdown_url'),
+        views.double_knockdown, name='double_knockdown_url'),
 
-    url(r'^secondary-scores/$', 'secondary_scores_search',
+    url(r'^secondary-scores/$', views.secondary_scores_search,
         name='secondary_scores_search_url'),
-    url(r'^secondary-scores/([^/]+)/([^/]+)/$', 'secondary_scores',
+    url(r'^secondary-scores/([^/]+)/([^/]+)/$', views.secondary_scores,
         name='secondary_scores_url'),
 
-    url(r'^image-categories/$', 'image_categories',
+    url(r'^image-categories/$', views.image_categories,
         name='image_categories_url'),
-    url(r'^image-category/([^/]+)/$', 'image_category',
+    url(r'^image-category/([^/]+)/$', views.image_category,
         name='image_category_url'),
-)
+]
