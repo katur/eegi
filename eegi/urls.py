f95c739a0b2107e402dc18d90761958095fe699b
Katherine Erickson <katherine.erickson@gmail.com>
2016-01-20 14:25:49 -0500

    Get rid of deprecated url patterns() from base url config

--- eegi/urls.py
+++ eegi/urls.py
@@ -1,20 +1,16 @@
-from django.conf.urls import include, patterns, url
+from django.conf.urls import include, url
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 
 
-admin.autodiscover()
-
-
-urlpatterns = patterns(
-    '',
-    url(r'', include('website.urls')),
-    url(r'', include('worms.urls')),
-    url(r'', include('clones.urls')),
-    url(r'', include('library.urls')),
-    url(r'', include('experiments.urls')),
-    url(r'^login/$', 'django.contrib.auth.views.login',
-        {'template_name': 'login.html'}, name='login_url'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout',
-        {'next_page': '/'}, name='logout_url'),
+urlpatterns = [
     url(r'^admin/', admin.site.urls),
-)
+    url(r'^login/$', auth_views.login, {'template_name': 'login.html'},
+        name='login_url'),
+    url(r'^logout/$', auth_views.logout, name='logout_url'),
+    url(r'^', include('website.urls')),
+    url(r'^', include('worms.urls')),
+    url(r'^', include('clones.urls')),
+    url(r'^', include('library.urls')),
+    url(r'^', include('experiments.urls')),
+]
