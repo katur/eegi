780bd69b629a486903c2de7a4e90e4dad2f3338f
Katherine Erickson <katherine.erickson@gmail.com>
2016-02-16 17:11:52 -0500

    Get rid of the management script for parsing the google dog

--- website/views.py
+++ website/views.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.shortcuts import render
 
 
@@ -9,9 +8,4 @@ def home(request):
 
 def help_page(request):
     """Render the help page."""
-    context = {
-        'batch_data_entry_gdoc_url': settings.BATCH_DATA_ENTRY_GDOC_URL,
-        'batch_data_entry_gdoc_name': settings.BATCH_DATA_ENTRY_GDOC_NAME,
-    }
-
-    return render(request, 'help.html', context)
+    return render(request, 'help.html')
