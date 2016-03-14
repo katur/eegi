c2c41f04208f8fbad09154dae7ca4692fd154a64
Katherine Erickson <katherine.erickson@gmail.com>
2016-03-14 02:12:30 -0400

    Simplify some worm queries

--- worms/helpers/queries.py
+++ worms/helpers/queries.py
@@ -8,6 +8,23 @@ def get_n2():
     return WormStrain.objects.get(pk='N2')
 
 
+def get_worms_for_screen_type(screen_type):
+    """
+    Get the worm strains involved in a particular screen.
+
+    screen_type must be 'ENH' or 'SUP'.
+    """
+    if screen_type not in ('ENH', 'SUP'):
+        raise Exception('screen_type must be ENH or SUP')
+
+    worms = WormStrain.objects
+
+    if screen_type == 'ENH':
+        return worms.exclude(permissive_temperature__isnull=True)
+    else:
+        return worms.exclude(restrictive_temperature__isnull=True)
+
+
 def get_worm_and_temperature_from_search_term(search_term, screen_type):
     """
     Get a tuple of (worm, temperature) matching search_term and screen_type.
@@ -19,28 +36,17 @@ def get_worm_and_temperature_from_search_term(search_term, screen_type):
     The combination of search_term/screen_type must uniquely identify a worm
     strain. Otherwise an exception is thrown.
     """
-    if screen_type != 'ENH' and screen_type != 'SUP':
-        raise Exception('screen_type must be ENH or SUP')
+    worms = get_worms_for_screen_type(screen_type)
 
-    if screen_type == 'ENH':
-        worms = (WormStrain.objects
-                 .filter(Q(gene=search_term) | Q(allele=search_term) |
+    worms = worms.filter(Q(gene=search_term) | Q(allele=search_term) |
                          Q(id=search_term))
-                 .exclude(permissive_temperature__isnull=True))
-    else:
-        worms = (WormStrain.objects
-                 .filter(Q(gene=search_term) | Q(allele=search_term) |
-                         Q(id=search_term))
-                 .exclude(restrictive_temperature__isnull=True))
 
     if not worms:
         return None
-
     elif len(worms) > 1:
         raise Exception('Multiple worm strains match search_term.')
-
     else:
-        worm = worms[0]
+        worm = worms.first()
 
     if screen_type == 'ENH':
         temperature = worm.permissive_temperature
@@ -48,19 +54,3 @@ def get_worm_and_temperature_from_search_term(search_term, screen_type):
         temperature = worm.restrictive_temperature
 
     return (worm, temperature)
-
-
-def get_worms_for_screen_type(screen_type):
-    """
-    Get the worm strains involved in a particular screen.
-
-    screen_type must be 'ENH' or 'SUP'.
-    """
-    worms = WormStrain.objects
-
-    if screen_type == 'ENH':
-        return worms.exclude(permissive_temperature__isnull=True)
-    elif screen_type == 'SUP':
-        return worms.exclude(restrictive_temperature__isnull=True)
-    else:
-        raise ValueError("screen_type must be 'ENH' or 'SUP'")
