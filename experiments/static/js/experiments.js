ef071e13855ae68f8c00e7765c1ce238fd27a96b
Katherine Erickson <katherine.erickson@gmail.com>
2016-07-20 22:32:29 -0400

    Start javascript keyboard shortcuts work

--- experiments/static/js/experiments.js
+++ experiments/static/js/experiments.js
@@ -1,6 +1,98 @@
 $(document).ready(function() {
-  return initializeCarousels();
+  initializeCarousels();
+  ScoringKeyboardShortcuts.init();
 });
+
+
+const DIRECTIONS = {
+  UP: "UP",
+  DOWN: "DOWN",
+}
+
+
+const KEYS = {
+  UP: 38,
+  DOWN: 40,
+}
+
+
+var ScoringKeyboardShortcuts = {
+  init: function() {
+    if (!$("#score-experiment-wells").length) {
+      return;
+    }
+
+    this.experiments = $(".experiment");
+    if (!this.experiments.length) {
+      return;
+    }
+
+    this.currentIndex = 0;
+    this.styleActiveExperiment($(this.experiments[this.currentIndex]));
+    this.listen();
+  },
+
+  listen: function() {
+    $("body").on("keydown", function(e) {
+      ScoringKeyboardShortcuts.handleKeyboardShortcut(e);
+    });
+  },
+
+  handleKeyboardShortcut: function(e) {
+    switch(e.which) {
+      case KEYS.UP:
+        e.preventDefault();
+        this.navigate(DIRECTIONS.UP);
+        break;
+      case KEYS.DOWN:
+        e.preventDefault();
+        this.navigate(DIRECTIONS.DOWN);
+        break;
+      default:
+        if (!this.isDigit(e.which)) {
+          return;
+        }
+        console.log("Digit " + this.getDigit(e.which));
+    }
+  },
+
+  navigate: function(direction) {
+    var nextIndex;
+    if (direction === DIRECTIONS.UP) {
+      nextIndex = this.currentIndex - 1;
+    } else if (direction === DIRECTIONS.DOWN) {
+      nextIndex = this.currentIndex + 1;
+    }
+
+    if (nextIndex < 0 || nextIndex >= this.experiments.length) {
+      return;
+    }
+
+    var nextExperiment = $(this.experiments[nextIndex]);
+    this.scrollToExperiment(nextExperiment);
+    this.currentIndex = nextIndex;
+  },
+
+  scrollToExperiment: function(experiment) {
+    this.styleActiveExperiment(experiment);
+    $("html, body").scrollTop(experiment.position().top);
+  },
+
+  styleActiveExperiment: function(experiment) {
+    $(this.experiments).removeClass("active");
+    experiment.addClass("active");
+  },
+
+  isDigit: function(key) {
+    return key >= 48 && key <= 57;
+  },
+
+  // Digit keys are 48 + digit
+  getDigit: function(key) {
+    return key - 48;
+  },
+
+};
 
 
 function initializeCarousels() {
